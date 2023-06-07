cf27bc8c7254a9532336cbe94d5d114fa5d9c53b
Art1903 <artemteliuk@gmail.com>
2023-06-07 13:47:28 +0300

    Display transactions, balance, total if user login

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const account1 = {
-  userName: 'Cecil Ireland',
+  userName: "Cecil Ireland",
   transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
   interest: 1.5,
   pin: 1111,
 };
 
 const account2 = {
-  userName: 'Amani Salt',
+  userName: "Amani Salt",
   transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
   interest: 1.3,
   pin: 2222,
 };
 
 const account3 = {
-  userName: 'Corey Martinez',
+  userName: "Corey Martinez",
   transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
   interest: 0.8,
   pin: 3333,
 };
 
 const account4 = {
-  userName: 'Kamile Searle',
+  userName: "Kamile Searle",
   transactions: [530, 1300, 500, 40, 190],
   interest: 1,
   pin: 4444,
 };
 
 const account5 = {
-  userName: 'Oliver Avila',
+  userName: "Oliver Avila",
   transactions: [630, 800, 300, 50, 120],
   interest: 1.1,
   pin: 5555,
@@ -36,29 +36,120 @@ const account5 = {
 const accounts = [account1, account2, account3, account4, account5];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.total__value--in');
-const labelSumOut = document.querySelector('.total__value--out');
-const labelSumInterest = document.querySelector('.total__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".total__value--in");
+const labelSumOut = document.querySelector(".total__value--out");
+const labelSumInterest = document.querySelector(".total__value--interest");
+const labelTimer = document.querySelector(".timer");
 
-const containerApp = document.querySelector('.app');
-const containerTransactions = document.querySelector('.transactions');
+const containerApp = document.querySelector(".app");
+const containerTransactions = document.querySelector(".transactions");
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseNickname = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseNickname = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayTransactions = (transactions) => {
+  containerTransactions.innerHTML = "";
 
+  transactions.forEach((trans, index) => {
+    const transType = trans > 0 ? "deposit" : "withdrawal";
+
+    const transactionsRow = `
+    <div class="transactions__row">
+          <div class="transactions__type transactions__type--${transType}">
+            ${index + 1} ${transType}
+          </div>
+          <div class="transactions__value">${trans}$</div>
+        </div>
+    `;
+    containerTransactions.insertAdjacentHTML("afterbegin", transactionsRow);
+  });
+};
+
+// Create nicknames accouts
+const createNicknames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.nickname = acc.userName
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word[0])
+      .join("");
+  });
+};
+
+createNicknames(accounts);
+
+const displayBalance = function (transactions) {
+  const balance = transactions.reduce((acc, trans) => acc + trans, 0);
+
+  labelBalance.textContent = `${balance}$`;
+};
+
+// Display Total deposits, withdrawals, interests
+const displayTotal = function (account) {
+  const depositesTotal = account.transactions
+    .filter((trans) => trans > 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumIn.textContent = `${depositesTotal}$`;
+
+  const withdrawalsTotal = account.transactions
+    .filter((trans) => trans < 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumOut.textContent = `${withdrawalsTotal}$`;
+
+  const interestTotal = account.transactions
+    .filter((trans) => trans > 0)
+    .map((depos) => (depos * account.interest) / 100)
+    .filter((interest, index, arr) => {
+      return interest >= 5;
+    })
+    .reduce((acc, interest) => acc + interest, 0);
+
+  labelSumInterest.textContent = `${interestTotal}$`;
+};
+
+let currentAccount;
+
+// Login in account and display informations
+
+btnLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    (account) => account.nickname === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    containerApp.style.opacity = 100;
+    // Display UI and welcome message
+    labelWelcome.textContent = `Раді що Ви знову з нами, ${
+      currentAccount.userName.split(" ")[0]
+    }!`;
+
+    // Clear inputs
+    inputLoginUsername.value = "";
+    inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    // Display transactions
+    displayTransactions(currentAccount.transactions);
+
+    // Display balance
+    displayBalance(currentAccount.transactions);
+
+    // Display tolal
+    displayTotal(currentAccount);
+  }
+});
